3ad82177e3b4322a4b1ee8c186fa095eeb27f042
ryuksan <clinicasystemlab@gmail.com>
2023-04-13 14:03:27 -0500

    queue

--- practica.js
+++ practica.js
@@ -1,3 +1,18 @@
-function Queue () {
+let y = 60;
+class Queue {
+  constructor() {
 
+    this.array = [];
+  }
+  meter(y) {
+    this.array.push(y);
+  }
+  sacar() {
+    this.array.shift();
+  }
+mayor (y) {
+  if (y > 59) {
+    this.array.unshift(y);
+  };
+}
 }
