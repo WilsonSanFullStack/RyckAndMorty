33600765b764ed1a29c6994ba0c22611f51265b9
ryuksan <clinicasystemlab@gmail.com>
2023-04-13 14:53:04 -0500

    grabriel

--- practica.js
+++ practica.js
@@ -1,21 +1,32 @@
+let y = 60;
 class Queue {
   constructor() {
 
     this.array = [];
   }
+  //la funcion meter agrea personas a la fila
   meter(persona) {
-    if (persona > 59) {
-        this.array.unshift(persona);}
+    if (persona > 59) {  //compara si la persona es mayor  
+        this.array.unshift(persona);}//si se cumple , agrega a la persona en primer lugar
         else{
-            this.array.push(persona);}
-  }
-  sacar() {
-    this.array.shift();
+            this.array.push(persona);}//si la funcion no se cumple agrega al ultimo lugar a la fila
+    }
+  sacar() {//el metodo sacar  elimina la primer persona de la fila respetando el "FIFO"
+   this.array.shift();
 }
 }
-<<<<<<< HEAD
-=======
-
+//declaramos una fila usando la clase constructiura Queue
 let fila=new Queue ;
+let menor=10;
+let mujer=26;
+let hombre=42;
+let anciano=70;
 
->>>>>>> 13a4a0cc16c82d449b469b7c176b60a0b90a7148
+fila.meter(menor)
+fila.meter(hombre)
+fila.meter(menor)
+fila.sacar()
+fila.meter(mujer)
+fila.meter(anciano)
+
+console.log(fila)
