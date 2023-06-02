f8b965deb173a0dbff4005cf2be2bf42519bd32c
ryuksan <clinicasystemlab@gmail.com>
2023-06-02 14:48:22 -0500

    actualizacion final

---
+++ ryckandmorty/Server/src/controllers/postFavs.js
@@ -0,0 +1,18 @@
+const {Favorite} = require('../DB_connection')
+
+module.exports = async (req, res) => {
+  try {
+    const {id, name, origin, status, image, species, gender} = req.body;
+
+    if(!name || !origin || !status || !image || !species || !gender) {
+      return res.status(401).send('Faltan datos')
+    }
+    await Favorite.findOrCreate({where: {name, origin, status, image, species, gender}});
+
+    const allFavorites = await Favorite.findAll();
+    return res.status(200).json(allFavorites);
+
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+};
--- ryckandmorty/Server/src/index.js
+++ ryckandmorty/Server/src/index.js
@@ -1,44 +1,7 @@
-const express = require('express');
-const server = express();
-const PORT = 3001
-const morgan = require('morgan');
-const router = require('./Routes/index')
+const server = require("./app");
+const PORT = 3001;
+const { conn } = require("./DB_connection");
 
+conn.sync({ force: false });
 
-server.use(express.json());
-server.use(morgan('dev'));
-
-
-
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-     'Access-Control-Allow-Headers',
-     'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-     'Access-Control-Allow-Methods',
-     'GET, POST, OPTIONS, PUT, DELETE'
-  );
-  next();
-});
-
-server.use('/rickandmorty', router );
-
-
-
-
-
-
-server.listen(PORT, ()=> console.log(`Listening on port: ${PORT}`))
-
-
-
-
-
-
-
-
-
-  
+server.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
